332b89da2252f2718f42f24caf7bef28eb10abc9
MrVeeJon <victor.jonathan.u@gmail.com>
2023-10-12 07:55:29 -0700

    README.md, lists.h, Task 0 created

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+*print_dlistint: Prints the elements of a list
+*dlistint_t: The structure that holds the list
+*@h: Pointer to the structure
+*Return: The number of nodes
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+    size_t node_count = 0;
+
+    while (h)
+    {
+        printf("%d\n", h->n);
+        h = h->next;
+        node_count++;
+    }
+
+    return node_count;
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,10 @@
+0x17. C - Doubly linked lists
+By: Julien Barbier
+Weight: 1
+Codes by Victor Jonathan Ukpongette
+
+The main objectives of this project is to enable me to know:
+
+What is a doubly linked list
+How to use doubly linked lists
+Start to look for the right source of information without too much help
